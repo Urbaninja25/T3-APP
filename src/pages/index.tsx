3e0acbca9904a92d889b563943fe8ff47e7058fa
Urbaninja25 <nbeka17@freeuni.edu.ge>
2023-10-27 17:14:51 +0400

    increase text size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -58,7 +58,7 @@ const Postview = (props: PostWithUser) => {
             post.createdAt,
           ).fromNow()}`}</span>
         </div>
-        <span className="text-2xl">{post.content}</span>
+        <span className="text-xl">{post.content}</span>
       </div>
     </div>
   );
