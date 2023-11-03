248af799cdccbb7cfae19dadaf2cdd8e7c548306
Urbaninja25 <nbeka17@freeuni.edu.ge>
2023-11-03 20:04:50 +0400

    resolving depandancy issues

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -9,9 +9,11 @@ import superjson from "superjson";
 import Head from "next/head";
 import { db } from "~/server/db";
 
+//The getServerSideProps function fetches the user data for the given slug and returns the data as props for the ProfileViewPage component.
 export async function getServerSideProps(
   context: GetServerSidePropsContext<{ slug: string }>,
 ) {
+  ////The createServerSideHelpers function creates a set of helpers that can be used to prefetch and fetch data from the API. The
   const helpers = createServerSideHelpers({
     router: appRouter,
     ctx: { db, userId: null },
@@ -21,17 +23,23 @@ export async function getServerSideProps(
   if (typeof slug !== "string") throw new Error("no slug");
 
   const username = slug.replace("@", "");
+
+  // The helpers.profile.getUserByUsername.prefetch function prefetches the user data for the given username.
+  //Prefetching the profile.getUserByUsername` query.
+  //`prefetch` does not return the result and never throws - if you need that behavior, use `fetch` instead.
+
   await helpers.profile.getUserByUsername.prefetch({ username });
-  // Make sure to return { props: { trpcState: helpers.dehydrate() } }
 
   return {
     props: {
+      //The helpers.dehydrate function dehydrates the TRPC state, which can be used to rehydrate the state on the client.
       trpcState: helpers.dehydrate(),
       username,
     },
   };
 }
 
+//The ProfileViewPage component uses the api.profile.getUserByUsername.useQuery hook to fetch the user data for the given username. If the data is not yet available, the component renders a loading indicator. If the data is not available, the component renders a 404 page. Otherwise, the component renders the profile page.
 export default function ProfileViewPage(
   props: InferGetServerSidePropsType<typeof getServerSideProps>,
 ) {
