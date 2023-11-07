ee2777a0a8985637de589b989dfcec484106be1a
Urbaninja25 <nbeka17@freeuni.edu.ge>
2023-11-07 18:25:36 +0400

    fixing data population route of  slug component

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -10,9 +10,6 @@ import Head from "next/head";
 import { db } from "~/server/db";
 import { PageLayout } from "~/componenets/layout";
 import Image from "next/image";
-import { useUser } from "@clerk/nextjs";
-import { filterUserForClient } from "~/server/helpers/filterUserForClient";
-
 // The getServerSideProps function is used to fetch data on the server and pass it to the component as props. This can be used to improve the performance of the application by prefetching data and rendering the page on the server.
 export async function getServerSideProps(
   context: GetServerSidePropsContext<{ slug: string }>,
