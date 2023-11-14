3b2f7d71a191b7397a25b08c6027453d3eb2f596
Urbaninja25 <nbeka17@freeuni.edu.ge>
2023-11-14 18:10:44 +0400

    fix postview componenet.whenever u click anything on it (excluding username) it goes to postview automatically

--- src/componenets/postView.tsx
+++ src/componenets/postView.tsx
@@ -1,11 +1,9 @@
 import type { RouterOutputs } from "~/utils/api";
-
 import dayjs from "dayjs";
-
 import Image from "next/image";
-
 import Link from "next/link";
 import relativeTime from "dayjs/plugin/relativeTime";
+import { useRouter } from "next/router";
 
 dayjs.extend(relativeTime);
 
@@ -14,8 +12,23 @@ type PostWithUser = RouterOutputs["post"]["getAll"][number];
 export const PostView = (props: PostWithUser) => {
   const { post, author } = props;
 
+  const router = useRouter();
+
+  const handlePostClick = async (event: React.MouseEvent<HTMLDivElement>) => {
+    const isUsernameLink = (event.target as HTMLElement).closest(
+      ".username-link",
+    );
+    if (!isUsernameLink) {
+      await router.push(`/post/${post.id}`);
+    }
+  };
+
   return (
-    <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
+    <div
+      key={post.id}
+      className="flex gap-3 border-b border-slate-400 p-4"
+      onClick={handlePostClick}
+    >
       <Image
         src={author.imageUrl}
         className="h-14 w-14 rounded-full"
@@ -24,11 +37,11 @@ export const PostView = (props: PostWithUser) => {
         height={56}
       />
       <div className="flex flex-col">
-        <div className="flex gap-1 text-slate-300">
-          <Link href={`/@${author.username}`}>
+        <div className=" flex gap-1 text-slate-300">
+          <Link href={`/@${author.username}`} className="username-link">
             <span>{`@${author.username} `}</span>
           </Link>
-          <Link href={`/post/${post.id}`}>
+          <Link href={`/post/${post.id} `} className="post-link">
             <span className="font-thin">{` · ${dayjs(
               post.created_at,
             ).fromNow()}`}</span>
